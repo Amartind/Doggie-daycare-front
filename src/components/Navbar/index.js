733884856a751425106bc08ece45e95c5b7a37f0
Amartind <112136243+Amartind@users.noreply.github.com>
2023-03-06 14:21:26 -0800

    added signed in and not signed in navbar. need conditionals

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,38 @@
+
 import React from "react";
 import {Link} from 'react-router-dom'
 import './style.css'
 
 const Navbar = () => {
+    
+    // still needs styling
+    // if no user logged in
     return(
         <div className="Navbar">
             <Link to="/">Home</Link> 
-            <div className="">
-            <Link to="/login">Login</Link> 
+            
+            <div className="notsignedin">
+                <Link to="/login">Login</Link>                      
+                <Link to="/signup">Sign-up</Link>            
+            </div> 
+            
                       
-            <Link to="/signup">Sign-up</Link>            
+        </div>
+    
+    )
+    // else
+        return(
+            <div className="Navbar">
+            <Link to="/">Home</Link> 
+            
+            <div className="signedin">
+                <Link to="/currentplaydates">Dogs in your area</Link>
+                <Link to="/createaplaydate">Make a meet up</Link>
+                <Link to="/profile">Profile</Link>
             </div>           
         </div>
-    )
+        )
+    
 }
 
 export default Navbar
