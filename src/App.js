d1249420d087a7a2a0d50a028e3dcdfa329cf362
Amartind <112136243+Amartind@users.noreply.github.com>
2023-03-07 10:00:01 -0800

    put forms in login and signup with states. still needs styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
+import Login from './components/login';
+import Signup from './components/signup';
 
 function App() {
   return (
@@ -13,8 +15,8 @@ function App() {
       <Route path='/addapooch' element={<h1>Add a dog</h1>}></Route>
       <Route path='/mydoggies' element={<h1>My dogs</h1>}></Route>
       <Route path='/profile' element={<h1>Profile</h1>}></Route>
-      <Route path='/login' element={<h1>Login</h1>}></Route>
-      <Route path='/signup' element={<h1>Signup</h1>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
       <Route path='*' element={<h1>404 Page not found</h1>}></Route>
     </Routes>
     <Footer/>
---
+++ src/components/login/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import './style.css'
+
+ 
+function Login() {
+  // Here we set two state variables for firstName and lastName using `useState`
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    return name === 'username' ? setUserName(value) : setPassword(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    setUserName('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <form className="loginform">
+        <input
+          value={username}
+          name="username"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+
+export default Login
