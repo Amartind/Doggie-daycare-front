dcd51669db1b4802c8daa0fff6f051e93573f470
Emoses95 <moses.e.collins@gmail.com>
2023-03-08 12:54:58 -0800

    added to mydoggies

---
+++ src/components/mydoggies/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './style.css'
+
+function MyDoggies() {
+    <div className="container">
+        <p> My Doggies</p>
+        <div>
+            {/* cloudinary photo */}
+        </div>
+    </div>
+}
