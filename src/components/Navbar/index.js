857da8e14b9d6f7030598033244d199b78761afc
Emoses95 <moses.e.collins@gmail.com>
2023-03-08 11:18:12 -0800

    added to createPlayDate

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,38 +1,38 @@
 
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 const Navbar = () => {
-    
+
     // still needs styling
     // if no user logged in
-    return(
+    return (
         <div className="Navbar">
-            <Link to="/">Home</Link> 
-            
+            <Link to="/">Home</Link>
+
             <div className="notsignedin ">
-                <Link to="/login" className="p-4">Login</Link>                      
-                <Link to="/signup" className="p-4">Sign-up</Link>            
-            </div> 
-            
-                      
+                <Link to="/login" className="p-4">Login</Link>
+                <Link to="/signup" className="p-4">Sign-up</Link>
+            </div>
+
+
         </div>
-    
+
     )
-    // else
-        return(
-            <div className="Navbar">
-            <Link to="/">Home</Link> 
-            
+    //  else
+    return (
+        <div className="Navbar">
+            <Link to="/">Home</Link>
+
             <div className="signedin">
                 <Link to="/currentplaydates">Dogs in your area</Link>
                 <Link to="/createaplaydate">Make a meet up</Link>
                 <Link to="/profile">Profile</Link>
-            </div>           
+            </div>
         </div>
-        )
-    
+    )
+
 }
 
 export default Navbar
--- src/components/createadate/index.js
+++ src/components/createadate/index.js
@@ -1,1 +1,89 @@
-import React from "react";
+import React, { useState } from 'react';
+import './style.css'
+
+function createPlayDate() {
+    const [eventTitle, setEventTitle] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [location, setLocation] = useState('');
+    const [dateNotes, setDateNotes] = useState('');
+
+    // Executing the value and name of the input on change
+    const handleInputChange = (e) => {
+
+        const { name, value } = e.target;
+
+        if (name === eventTitle) {
+            return setEventTitle(value)
+        } else if (name === date) {
+            return setDate(value)
+        } else if (name === time) {
+            return setTime(value)
+        } else if (name === location) {
+            return setLocation(value)
+        } else if (name === dateNotes) {
+            return setDateNotes(value)
+        }
+    };
+
+    // Prevents refreshing the page a default behavior
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="container">
+            <p>Make a Play Date</p>
+            <form className="dogform grid p-2 grid-cols-2 grid-rows-6">
+                <div className="grid gap-4 grid-cols-1 grid-rows-7">
+                    <input
+                        value={eventTitle}
+                        name="eventTitle"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder='Event Title'
+                    />
+                    <div className="grid gap-4 grid-cols-1 grid-rows-7">
+                        <input
+                            value={date}
+                            name="date"
+                            onChange={handleInputChange}
+                            type='text'
+                            placeholder='Date' />
+                    </div>
+                    <div className="grid gap-4 grid-cols-1 grid-rows-7">
+                        <input
+                            value={time}
+                            name='time'
+                            onChange={handleInputChange}
+                            type="text"
+                            placeholder='Time' />
+                    </div>
+                    <div>
+                        <input
+                            value={location}
+                            name='location'
+                            onChange={handleInputChange}
+                            type="text"
+                            placeholder='Location' />
+                    </div>
+                    <div className="grid gap-4 grid-cols-1 grid-rows-7">
+                        <input
+                            value={dateNotes}
+                            name='dateName'
+                            onChange={handleInputChange}
+                            type="text"
+                            placeholder='Date Notes'
+                        />
+                    </div>
+                </div>
+                <button type="button" onClick={handleFormSubmit}>Send</button>
+            </form>
+
+
+
+        </div>
+    )
+}
+
+export default createPlayDate
