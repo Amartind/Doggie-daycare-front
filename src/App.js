9c1c0f8a68bf39727f210043b11a8af8edff977b
Aly-gator <119552961+Kawaikimono@users.noreply.github.com>
2023-03-10 14:08:46 -0800

    trying to checkout me branch

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import Profile from './components/profile';
 import Addpooch from './components/addapooch';
 import CreateADate from './components/createadate';
 import Currentdates from './components/currentplaydates';
+import Dashboard from './components/dashboard';
+import Hompage from './components/homepage';
+
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
     <Routes>
-      <Route path='/' element={<h1>Dashboard</h1>}></Route>
+      <Route path='/' element={<Hompage/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
       <Route path='/createaplaydate' element={<CreateADate/>}></Route>
       <Route path='/currentplaydates' element={<Currentdates/>}></Route>
       <Route path='/addapooch' element={<Addpooch/>}></Route>
@@ -21,8 +25,7 @@ function App() {
       <Route path='/profile' element={<Profile/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
       <Route path='/signup' element={<Signup/>}></Route>
-      <Route path='/signup' element={<Signup/>}></Route>
-      <Route path='*' element={<h1>404 Page not found</h1>}></Route>
+      <Route path='*' element={<Hompage/>}></Route>
     </Routes>
     <Footer/>
     </BrowserRouter>
