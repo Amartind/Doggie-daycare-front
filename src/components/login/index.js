3702d80d9fdf9c0a865e2488a7ae3157a9d5d3bc
Emoses95 <moses.e.collins@gmail.com>
2023-03-12 21:40:37 -0700

    fixed src login

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -41,31 +41,31 @@ function Login() {
     setPassword('');
   };
 
-};
 
-return (
-  <div className="container">
-    <form className="loginform grid gap-4 grid-cols-1 grid-rows-2">
-      <input
-        value={username}
-        name="username"
-        onChange={handleInputChange}
-        type="text"
-        placeholder="Username"
-      />
-      <input
-        value={password}
-        name="password"
-        onChange={handleInputChange}
-        type="password"
-        placeholder="Password"
-      />
-      <button type="button" onClick={handleFormSubmit}>
-        Submit
-      </button>
-    </form>
-  </div>
-);
+  return (
+    <div className="container">
+      <form className="loginform grid gap-4 grid-cols-1 grid-rows-2">
+        <input
+          value={username}
+          name="username"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 
 
