f1733f331e5b154550ae61a9e3067dcf233efca6
Emoses95 <moses.e.collins@gmail.com>
2023-03-13 09:33:41 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
 import Login from './components/login';
@@ -18,27 +18,27 @@ function App() {
   // const user = await fetch(localhost/api/users/5)
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Homepage/>}></Route>
-      <Route path='/dashboard' element={<Dashboard/>}></Route>
-      <Route path='/createaplaydate' element={<CreateADate/>}></Route>
-      <Route path='/currentplaydates' element={<Currentdates/>}></Route>
-      <Route path='/addapooch' element={<Addpooch/>}></Route>
-      <Route path='/localpups' element={<Localpups/>}></Route>
-      <Route path='/mydoggies' element={<MyDoggies/>}></Route>
-      <Route path='/profile' element={<Profile/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/signup' element={<Signup/>}></Route>
-      <Route path='*' element={<Homepage/>}></Route>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Homepage />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/createaplaydate' element={<CreateADate />}></Route>
+        <Route path='/currentplaydates' element={<Currentdates />}></Route>
+        <Route path='/addapooch' element={<Addpooch />}></Route>
+        <Route path='/localpups' element={<Localpups />}></Route>
+        <Route path='/mydoggies' element={<MyDoggies />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='*' element={<Homepage />}></Route>
+      </Routes>
 
-    <Link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <Link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet"/>
-    <Link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/luxonauta/luxa@1.1/dist/expanded/luxa.css"/>
-    <Link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/luxonauta/luxa@1.1/dist/compressed/luxa.css"/>
-    <Footer/>
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <Link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet" />
+      <Link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/luxonauta/luxa@1.1/dist/expanded/luxa.css" />
+      <Link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/luxonauta/luxa@1.1/dist/compressed/luxa.css" />
+      <Footer />
 
     </BrowserRouter>
   );
