47cea3f624c51c7b0e65dd08c8a28ba6d61a8b23
Amartind <112136243+Amartind@users.noreply.github.com>
2023-03-14 12:51:54 -0700

    fixing mistake

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+import API from "../../utils/API";
+import './style.css'
+import { useParams } from "react-router-dom";
+import LOGO from "./LogoDogs.png"
+
+const Navbar = (props) => {
+    const params = useParams();
+    console.log(params)
+    const [Loggedin, setLoggedin] = useState({})
+    function hello() {
+        props.setIsLoggedIn(false);
+    }
+    return (
+
+        <div >
+            <img className="logo" src={LOGO} alt="Doggie Dates" />
+            {/* <p className="text-5xl flex justify-center flex-auto">Doggie Daycare</p> */}
+            <div className="Navbar grid grid-rows-1 grid-cols-2 flex-auto">
+                {/* <h2 className="navlink flex-auto">Welcome {user.name}</h2> */}
+                <Link to="/" className="navlink p-2 flex-auto horizontal up">Home</Link>
+
+                {props.isLoggedIn ? <div className="signedin justify-self-end flex-auto">
+                    <Link to="/localpups" className="p-4 navlink flex-auto">Local Pups</Link>
+                    <Link to="/currentplaydates" className="p-4 navlink flex-auto">Local Meetups</Link>
+                    <Link to="/createaplaydate" className="p-4 navlink flex-auto">Make a meet up</Link>
+                    <div className="dropdown">
+                        <button className="dropbtn">Profile</button>
+                        <div className="dropdown-content ">
+                            <div className="grid grid-rows-4">
+                                <Link to={`/profile/${props.userId}`} className="dropdownlink p-4 navlink flex-auto" >Edit Profile</Link>
+                                <Link to="/mydoggies" className="dropdownlink p-4 navlink flex-auto">My Dogs</Link>
+                                <Link to="/addapooch" className="dropdownlink p-4 navlink flex-auto" >Add Dog</Link>
+                                <Link to="/" className="dropdownlink p-4 navlink flex-auto" onClick={hello}>Logout</Link>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    :
+
+                    <div className="notsignedin justify-self-end down flex-auto">
+                        <Link to="/login" className="p-4 navlink flex-auto">Login</Link>
+                        <Link to="/signup" className="p-4 navlink flex-auto">Sign-up</Link>
+                    </div>
+                }
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Navbar
---
+++ src/components/profile/index.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { editOwner, getAllOwners } from "../../utils/API";
+import MyDoggies from '../mydoggies';
+import './style.css'
+
+
+
+function Profile(props) {
+  const params = useParams();
+  const [user, setUser] = useState({});
+  const [isMyPage, setIsMyPage] = useState(false);
+  const [fullname, setFullName] = useState('');
+  const [username, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const fetchUser = () => {
+    getAllOwners(params.id).then((data) => {
+      setUser(data);
+      console.log(data)
+      console.log(props.userId);
+      if (props.userId == params.id) {
+        setIsMyPage(true);
+      } else {
+        setIsMyPage(false);
+      }
+    })
+  }
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    if (name === 'username') {
+      return setUserName(value)
+    } else if (name === 'fullname') {
+      return setFullName(value)
+    } else if (name === 'email') {
+      return setEmail(value)
+    }else if (name === 'password') {
+      return setPassword(value)
+    } else if (name === 'phone') {
+      return setPhone(value)
+    } else if (name === 'address') {
+      return setAddress(value)
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+    editOwner(
+    {
+      name:setFullName()||user.name,
+      email:setEmail()||user.email,
+      phone:setPhone()||user.phone,
+      password:setPassword()||user.password,
+      username:setUserName()||user.username,
+      address:setAddress()||user.address
+    },
+    user.id,
+    localStorage.getItem("token")
+    ).then((data) =>{
+      console.log(data)
+    })
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, [props.userId, params.id]);
+
+  return (
+    <div className="container">
+      <p>Welcome {fullname}</p>
+      <form className="signupform grid gap-4 grid-cols-1 grid-rows-6">
+        <input
+          value={fullname}
+          name="fullname"
+          onChange={handleInputChange}
+          type="text"
+          placeholder={user.fullname}
+          className="inputfield"
+        />
+        <input
+          value={username}
+          name="username"
+          onChange={handleInputChange}
+          type="text"
+          placeholder={user.username}
+          className="inputfield"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder={user.email}
+          className="inputfield"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+          className="inputfield"
+        />
+        <input
+          value={phone}
+          name="phone"
+          onChange={handleInputChange}
+          type="text"
+          placeholder={user.phone}
+          className="inputfield"
+        />
+        <input
+          value={address}
+          name="address"
+          onChange={handleInputChange}
+          type="text"
+          placeholder={user.address}
+          className="inputfield"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+
+
+}
+
+export default Profile;
