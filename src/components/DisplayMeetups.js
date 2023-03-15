d5dee00cd58bf94cc280e331a6a9d9f26473d5ea
Jason A. Arter <jason.arter@gmail.com>
2023-03-15 14:05:55 -0700

    Search page now displaying results!

--- src/components/DisplayMeetups.js
+++ src/components/DisplayMeetups.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function DisplayMeetup (props) {
+export default function DisplayMeetup(props) {
     console.log(props.Meetup)
     return (
-    <div className='projectBox'>
-        <h4>{props.Meetup.name}</h4>
-        <p>{props.Meetup.dateTime}</p>
-        <p>{props.Meetup.address}</p>
-        <p>{props.Meetup.description}</p>
-        {/* <h5>Pups Hosting</h5> */}
-        {/*props.Meetup.Pets.map(Pet=>{
+        <div className='projectBox'>
+            <h4>{props.Meetup.name}</h4>
+            <p>{props.Meetup.dateTime}</p>
+            <p>{props.Meetup.address}</p>
+            <p>{props.Meetup.description}</p>
+            {/* <h5>Pups Hosting</h5> */}
+            {/*props.Meetup.Pets.map(Pet=>{
             <>
             <p>{Pet.name}</p>
             <p>{Pet.gender}</p>
@@ -20,6 +20,6 @@ export default function DisplayMeetup (props) {
             <p>{Pet.vaccinated}</p>
             </>
         })*/}
-    </div>
-  )
+        </div>
+    )
 }
