6abceec4cd2da81403684ecc87874189306e0014
Emoses95 <moses.e.collins@gmail.com>
2023-03-14 19:42:56 -0700

    added imports and useState

--- src/components/mydoggies/index.js
+++ src/components/mydoggies/index.js
@@ -1,51 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getAllPets } from "../../utils/API"
 import './style.css';
 
 function MyDoggies(props) {
-    const [user, setUser] = useState({});
-    const [petsName, setPetsName] = useState('');
-    const [petsGender, setPetsGender] = useState('');
-    const [petsAge, setPetsAge] = useState('');
-    const [petsBreed, setPetsBreed] = useState('');
-    const [petsPersonality, setPetsPersonality] = useState('');
-    const [petsSnipsnip, setPetsSnipsnip] = useState('');
-    const [petsVaccinated, setPetsVaccinated] = useState('');
-    const fetchUser = () => {
-        getAllPets(params.id)
-    }
+    const { id } = useParams();
+    const [pets, setPets] = useState([]);
+    const [isMyPage, setIsMyPage] = useState(false);
 
+    useEffect(() => {
+        const fetchPets = async () => {
+            try {
+                const pets = await getAllPets(id);
+                setPets(pets);
+                setIsMyPage(props.userId === id);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchPets();
+    }, [id, props.userId]);
 
     const handleDelete = (id) => {
-        // create a new array with all dogs except for the one with the matching id
         const updatedPets = pets.filter((pet) => pet.id !== id);
-        // call a function to update the state of the dogs in the parent component
+        setPets(updatedPets);
         props.updatePets(updatedPets);
     };
 
     return (
         <div className="mydoggies-container">
-            <p className='flex justify-center text-lg'> My Doggies</p>
+            <p className="flex justify-center text-lg"> My Doggies</p>
             <div className="dog-container">
-                {pets && pets.map((dog) => (
-                    <div className="dog" key={dog.id}>
-                        <div className="dog-info">
-                            <p>Dog Name: {pets.name}</p>
-                            <p>Gender: {pets.gender}</p>
-                            <p>Age: {pets.age}</p>
-                            <p>Breed: {pets.breed}</p>
-                            <p>Personality: {pets.personality}</p>
-                            <p>Snip Snip: {pets.snipsnip}</p>
-                            <p>Vaccinated: {pets.vaccinated}</p>
-                            <button onClick={() => handleDelete(pets.id)}>Delete</button>
+                {pets &&
+                    pets.map((pet) => (
+                        <div className="dog" key={pet.id}>
+                            <div className="dog-info">
+                                <p>Dog Name: {pet.name}</p>
+                                <p>Gender: {pet.gender}</p>
+                                <p>Age: {pet.age}</p>
+                                <p>Breed: {pet.breed}</p>
+                                <p>Personality: {pet.personality}</p>
+                                <p>Snip Snip: {pet.snipsnip}</p>
+                                <p>Vaccinated: {pet.vaccinated}</p>
+                                <button onClick={() => handleDelete(pet.id)}>Delete</button>
+                            </div>
                         </div>
-                    </div>
-                ))} */}
+                    ))}
             </div>
         </div>
     );
 }
 
-
 export default MyDoggies;
-
-
