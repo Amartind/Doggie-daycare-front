020f80a3283f7399eaff86c6ccdb0240bff2a490
Emoses95 <moses.e.collins@gmail.com>
2023-03-15 15:41:20 -0700

    styling added to mydoggiesCard

--- src/components/mydoggies/index.js
+++ src/components/mydoggies/index.js
@@ -14,23 +14,14 @@ function MyDoggies(props) {
             setUser(data);
         })
     }
-    // const fetchUser = () => {
-    //     getAllOwners(params.id).then(data => {
-    //         setUser(data)
-    //         console.log(user.Pets)
-    //     })
-    // }
-    // const handleDelete = (id) => {
-    //     const updatedPets = pets.filter((pet) => pet.id !== id);
-    //     setPets(updatedPets);
-    //     deletepet(id of de);
-    // };
+
     useEffect(() => {
         fetchUser();
     }, []);
     return (
+
         <div className="mydoggies-container">
-            <h1> My Doggies</h1>
+
             {user.username ? (
                 user?.Pets?.map(pets => <MydoggiesCard
                     key={pets.petsId}
@@ -48,30 +39,3 @@ function MyDoggies(props) {
     );
 }
 export default MyDoggies
-// const { id } = useParams();
-// const [pets, setPets] = useState([]);
-// const [isMyPage, setIsMyPage] = useState(false);
-// const [petsName, setPetsName] = useState('');
-// const [petsGender, setPetsGender] = useState('');
-// const [petsAge, setPetsAge] = useState('');
-// const [petsBreed, setPetsBreed] = useState('');
-// const [petsPersonality, setPetsPersonality] = useState = ('');
-// const [snipSnip, setSnipSnip] = useState('');
-// const [vaccinated, setVaccinated] = useState('');
-{/* <p className="flex justify-center text-lg"> My Doggies</p>
-            <div className="dog-container">
-                {user ? Pets ? map((pet) => (
-                    <div className="dog" key={pet.id}>
-                        <div className="dog-info">
-                            <p>Dog Name: {petsName}</p>
-                            <p>Gender: {petsGender}</p>
-                            <p>Age: {petsAge}</p>
-                            <p>Breed: {petsBreed}</p>
-                            <p>Personality: {petsPersonality}</p>
-                            <p>Snip Snip: {snipSnip}</p>
-                            <p>Vaccinated: {vaccinated}</p>
-                            <button onClick={() => handleDelete(pet.id)}>Delete</button>
-                        </div>
-                    </div>
-                ))}
-            </div> */}
--- src/components/mydoggiesCard/index.js
+++ src/components/mydoggiesCard/index.js
@@ -5,17 +5,38 @@ import './style.css';
 const MydoggiesCard = (props) => {
 
     return (
-        <div>
-            <h3>{props.name}</h3>
-            name={props.name}
-            gender={props.gender}
-            age={props.age}
-            breed={props.breed}
-            personality={props.personality}
-            snipsnip={props.spayed_neutered}
-            vaccinated={props.vaccinated}
+        <div className="mydoggies-card">
+            <div className="mydoggies-card__header">
+                <h3>{props.name}</h3>
+                <span className="mydoggies-card__header__breed">{props.breed} </span>
+            </div>
+            <div className="mydoggies-card__body">
+                <div className="mydoggies-card__body__info">
+                    <div className="mydoggies-card__body__info__row">
+                        <span className="mydoggies-card__body__info__label">Gender: </span>
+                        <span className="mydoggies-card__body__info__value">{props.gender} </span>
+                    </div>
+                    <div className="mydoggies-card__body__info__row">
+                        <span className="mydoggies-card__body__info__label">Age: </span>
+                        <span className="mydoggies-card__body__info__value">{props.age} </span>
+                    </div>
+                    <div className="mydoggies-card__body__info__row">
+                        <span className="mydoggies-card__body__info__label">Personality: </span>
+                        <span className="mydoggies-card__body__info__value">{props.personality} </span>
+                    </div>
+                </div>
+                <div className="mydoggies-card__body__status">
+                    <div className="mydoggies-card__body__status__item">
+                        <span className="mydoggies-card__body__status__label">✂️: </span>
+                        <span className={`mydoggies-card__body__status__value ${props.spayed_neutered ? 'mydoggies-card__body__status__value--yes' : 'mydoggies-card__body__status__value--no'}`}>{props.spayed_neutered ? 'Yes' : 'No'}</span>
+                    </div>
+                    <div className="mydoggies-card__body__status__item">
+                        <span className="mydoggies-card__body__status__label">💉: </span>
+                        <span className={`mydoggies-card__body__status__value ${props.vaccinated ? 'mydoggies-card__body__status__value--yes' : 'mydoggies-card__body__status__value--no'}`}>{props.vaccinated ? 'Yes' : 'No'}</span>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 }
 
